54e4ff465d2598e6552c6cfee9a55c07ebba7c8c
nngvn8 <smartin-online@web.de>
2021-02-26 20:29:28 +0100

    clean jumping, movement and ground colission

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.init()
 blockAmount = 12
 windowWidth = 1200
 windowHeight = 600
-Acceleration = 1
+gravity = 1
 
 
 window = pygame.display.set_mode((windowWidth, windowHeight))
@@ -24,22 +24,54 @@ class Player:
         self.playerYC = playerYC
         self.playerMS = playerMS
         self.jumpSpeed = jumpSpeed
-        self.inJump = False
+        self.curSpeedX = 0
+        self.curSpeedY = 0
+        # self.inJump = False
         self.displayPlayer()
 
     def displayPlayer(self):
         self.playerRect = pygame.Rect((self.playerXC, self.playerYC, self.playerWidth, self.playerHeight))
         pygame.draw.rect(window, (255, 255, 255), self.playerRect)
 
-    def move(self, direction):
-        if direction > 0:
-            self.playerXC += self.playerMS
-        else:
-            self.playerXC -= self.playerMS
+    def move(self):
 
-    def jump(self):
-        self.playerYC -= self.jumpSpeed
-        self.jumpSpeed -= Acceleration
+        oldYpos = self.playerYC
+        oldXpos = self.playerXC
+
+        if self.curSpeedX > 0:
+            xStep = 1
+        elif self.curSpeedX < 0:
+            xStep = -1
+
+        if self.curSpeedY > 0:
+            yStep = 1
+        elif self.curSpeedY < 0:
+            yStep = -1
+
+        for i in range(toUnsigned(self.curSpeedX)):
+            if (ground.groundCollisionX() == 0):
+                self.playerXC += xStep
+            else:
+                self.playerXC -= xStep
+                break
+
+        for i in range(toUnsigned(self.curSpeedY)):
+            if (ground.groundCollisionY() == 0):
+                self.playerYC += yStep
+            else:
+                self.playerYC -= 1
+                break
+
+        if toUnsigned(oldXpos - self.playerXC) == 1: self.playerXC = oldXpos
+        if toUnsigned(oldYpos - self.playerYC) == 1: self.playerYC = oldYpos
+
+
+    def gravity(self):
+        self.curSpeedY += gravity
+
+    # def jump(self):
+      #  self.playerYC -= self.jumpSpeed
+      #  self.jumpSpeed -= Acceleration
 
 
 class GroundBlock:
@@ -68,12 +100,34 @@ class Ground:
         for i in range(self.blockAmount+1):
             self.groundArray.append(GroundBlock(self.blockWidth, randint(5,200), self.blockWidth*i))
 
-    def groundCollision(self):
+    def groundCollisionY(self):
         for i in range(self.blockAmount+1):
-            if(Player1.playerRect.colliderect(self.groundArray[i].blockRect)):
-                Player1.inJump = False
-                Player1.playerYC = windowHeight-self.groundArray[i].blockHeight-Player1.playerHeight
-                return True
+            if Collision("b", i):
+                if Collision("y", i):
+                    Player1.curSpeedY = 0
+                    # Player1.playerYC = windowHeight-self.groundArray[i].blockHeight-Player1.playerHeight
+                    return 1
+        return 0
+
+    def groundCollisionX(self):
+        for i in range(self.blockAmount+1):
+            if Collision("b", i):
+                if Collision("x", i):
+                    Player1.curSpeedX = 0
+                    # Player1.playerYC = windowHeight-self.groundArray[i].blockHeight-Player1.playerHeight
+                    return 1
+        return 0
+
+
+def Collision(mode, i):
+    if mode == "b" or mode == "B":
+        return Player1.playerYC + Player1.playerHeight > windowHeight - ground.groundArray[i].blockHeight and Player1.playerYC < windowHeight and Player1.playerXC + Player1.playerWidth > ground.groundArray[i].blockX and Player1.playerXC < ground.groundArray[i].blockX + ground.groundArray[i].blockWidth
+    if mode == "x" or mode == "X":
+        return Player1.playerYC + Player1.playerHeight > windowHeight - ground.groundArray[i].blockHeight and Player1.playerYC < windowHeight
+    if mode == "y" or mode == "Y":
+        return Player1.playerXC + Player1.playerWidth > ground.groundArray[i].blockX and Player1.playerXC < ground.groundArray[i].blockX + ground.groundArray[i].blockWidth
+
+
 
 
 def toUnsigned(x):
@@ -89,36 +143,28 @@ Player1 = Player(80, 40, 200, 200, 5, 15)
 
 
 while True:
-    if ground.groundCollision():
-        Player1.jumpSpeed = 15
-
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: pygame.quit()
 
     Player1.displayPlayer()
-    # displayPlayer()
 
     keysPressed = pygame.key.get_pressed()
 
     if keysPressed[pygame.K_RIGHT]:
-        Player1.move(1)
-    if keysPressed[pygame.K_LEFT]:
-        Player1.move(-1)
+        Player1.curSpeedX = Player1.playerMS
+    elif keysPressed[pygame.K_LEFT]:
+        Player1.curSpeedX = -Player1.playerMS
+    else:
+        Player1.curSpeedX = 0
     if keysPressed[pygame.K_SPACE]:
-        Player1.inJump = True
+        Player1.curSpeedY = -Player1.jumpSpeed
+        Player1.playerYC -= 5
 
-    if Player1.inJump:
-       Player1.jump()
-     #   for i in range(toUnsigned(Player1.jumpSpeed)):
-      #      if (ground.groundCollision()):
-       #         Player1.jumpSpeed = 15
-        ##        break
-           # if Player1.jumpSpeed > 0:
-          #      Player1.playerYC -= 1
-            #elif Player1.jumpSpeed < 0:
-            #    Player1.playerYC += 1
+    Player1.gravity()
+
+    Player1.move()
+
 
     for i in range(blockAmount+1):
         ground.groundArray[i].drawBlock()
